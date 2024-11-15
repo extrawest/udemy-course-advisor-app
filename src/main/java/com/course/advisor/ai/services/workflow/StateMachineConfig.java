8d0c10b35b987c309d3a5cda23bf49f18f951ed8
yevhen-ruban-ew <eugene.ruban@sintez.co.za>
2024-11-15 18:23:42 +0200

    workflow configuration

--- src/main/java/com/course/advisor/ai/services/workflow/StateMachineConfig.java
+++ src/main/java/com/course/advisor/ai/services/workflow/StateMachineConfig.java
@@ -1,5 +1,7 @@
 package com.course.advisor.ai.services.workflow;
 
+import com.course.advisor.ai.services.agents.CVExtractionAgent;
+import com.course.advisor.ai.services.agents.CurseRecommendationAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -19,19 +21,12 @@ class StateMachineConfig extends StateMachineConfigurerAdapter<States, Events> {
 
     private static final Logger log = LoggerFactory.getLogger(StateMachineConfig.class);
 
-    private final RequirementsEvaluator requirementsEvaluator;
-    private final ScriptGenerator scriptGenerator;
-    private final SolutionVerifier solutionVerifier;
-    private final RequirementsRewriter requirementsRewriter;
+    private final CurseRecommendationAgent curseRecommendationAgent;
+    private final CVExtractionAgent cvExtractionAgent;
 
-    StateMachineConfig(RequirementsEvaluator requirementsEvaluator,
-            ScriptGenerator scriptGenerator,
-            SolutionVerifier solutionVerifier,
-            RequirementsRewriter requirementsRewriter) {
-        this.requirementsEvaluator = requirementsEvaluator;
-        this.scriptGenerator = scriptGenerator;
-        this.solutionVerifier = solutionVerifier;
-        this.requirementsRewriter = requirementsRewriter;
+    StateMachineConfig(CurseRecommendationAgent curseRecommendationAgent, CVExtractionAgent cvExtractionAgent) {
+        this.curseRecommendationAgent = curseRecommendationAgent;
+        this.cvExtractionAgent = cvExtractionAgent;
     }
 
     @Override
@@ -39,8 +34,8 @@ class StateMachineConfig extends StateMachineConfigurerAdapter<States, Events> {
         states
                 .withStates()
                 .initial(States.AWAITING_INPUT)
-                .state(States.CS_DATA_EXTRACTION, evaluateRequirementsAction())
-                .state(States.RESULT_GENERATION, generateScriptAction())
+                .state(States.CS_DATA_EXTRACTION, extractCvData())
+                .state(States.RESULT_GENERATION, generateResult())
                 .end(States.SUCCESSFUL_COMPLETION)
                 .end(States.FAILED_COMPLETION);
     }
@@ -56,11 +51,11 @@ class StateMachineConfig extends StateMachineConfigurerAdapter<States, Events> {
                 .withExternal().source(States.SOLUTION_VERIFICATION).target(States.RESULT_GENERATION).event(Events.SOLUTION_REJECTED);
     }
 
-    private Action<States, Events> evaluateRequirementsAction() {
+    private Action<States, Events> extractCvData() {
         return stateContext -> {
             log.info("Evaluating requirements...");
             var requirements = getVariable(stateContext, Variables.CV_DATA);
-            if (requirementsEvaluator.areRequirementsFeasible(requirements)) {
+            if (cvExtractionAgent.answer(requirements)) {
                 sendEvent(stateContext.getStateMachine(), Events.REQUIREMENTS_EVALUATED);
             } else {
                 sendEvent(stateContext.getStateMachine(), Events.REQUIREMENTS_REJECTED);
@@ -68,37 +63,13 @@ class StateMachineConfig extends StateMachineConfigurerAdapter<States, Events> {
         };
     }
 
-    private Action<States, Events> generateScriptAction() {
+    private Action<States, Events> generateResult() {
         return stateContext -> {
             log.info("Generating script...");
             var requirements = getVariable(stateContext, Variables.CV_DATA);
-            var script = scriptGenerator.generateScript(requirements);
+            var script = curseRecommendationAgent.answer(requirements);
             stateContext.getExtendedState().getVariables().put(Variables.RESULT, script);
             sendEvent(stateContext.getStateMachine(), Events.SCRIPT_GENERATED);
-        };
-    }
-
-    private Action<States, Events> verifySolutionAction() {
-        return stateContext -> {
-            log.info("Verifying solution...");
-            var requirements = getVariable(stateContext, Variables.CV_DATA);
-            var script = getVariable(stateContext, Variables.RESULT);
-            if (solutionVerifier.isScriptValid(requirements, script)) {
-                sendEvent(stateContext.getStateMachine(), Events.SOLUTION_VERIFIED);
-            } else {
-                sendEvent(stateContext.getStateMachine(), Events.SOLUTION_REJECTED);
-            }
-        };
-    }
-
-    private Action<States, Events> rewriteRequirementsAction() {
-        return stateContext -> {
-            log.info("Rewriting requirements...");
-            var requirements = getVariable(stateContext, Variables.CV_DATA);
-            var script = getVariable(stateContext, Variables.RESULT);
-            var rewrittenRequirements = requirementsRewriter.rewriteRequirements(requirements, script);
-            stateContext.getExtendedState().getVariables().put(Variables.CV_DATA, rewrittenRequirements);
-            sendEvent(stateContext.getStateMachine(), Events.REQUIREMENTS_REWRITTEN);
         };
     }
 
